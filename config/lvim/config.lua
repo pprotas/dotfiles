1c3de5e37e86a35ffec4f9f3ec655aa70e0ded9c
Pawel Protas <pawel@hackerone.com>
2022-10-19 19:22:22 +0200

    feat(lvim, tmux): added lvim/tmux integration

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -332,6 +332,27 @@ lvim.plugins = {
   { "zbirenbaum/copilot-cmp",
     after = { "copilot.lua", "nvim-cmp" },
   },
+  {
+    "aserowy/tmux.nvim",
+    config = function()
+      require("tmux").setup {
+        navigation = {
+          -- cycles to opposite pane while navigating into the border
+          cycle_navigation = true,
+
+          -- enables default keybindings (C-hjkl) for normal mode
+          enable_default_keybindings = true,
+
+          -- prevents unzoom tmux when navigating beyond vim border
+          persist_zoom = true,
+        },
+        resize = {
+          -- enables default keybindings (A-hjkl) for normal mode
+          enable_default_keybindings = true,
+        },
+      }
+    end,
+  }
 }
 
 lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"
