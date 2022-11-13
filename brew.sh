c17d7b4b70200695f705341c3105c21e5aadac7f
Pawel Protas <pawel@hackerone.com>
2022-11-13 15:56:21 +0100

    feat(install): brew install script correctly sources now

--- brew.sh
+++ brew.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
 echo "[brew] INFO: Installation started"
 # Set-up
 # ======
 # Install Homebrew if it's not installed already
-if ! brew_loc="$(type -p brew)" || [[ -z "$brew_loc" ]]; then
+if ! brew_loc="$(whence -p brew)" || [[ -z "$brew_loc" ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
