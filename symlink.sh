bde1bd5c09766801bf50694c49d303af783cfcfd
Pawel Protas <pawel@hackerone.com>
2022-11-13 13:15:32 +0100

    feat(install): symlink script

---
+++ symlink.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Symlinks the .files directory to the home directory
+
+# Method to handle errors
+function error {
+    echo "ERROR: $1" >&2
+    exit 2
+}
+
+# Check if oh-my-zsh is installed, and error out if it isn't.
+# This is necessary because oh-my-zsh messes up .zshrc
+if ! [[ -d ~/.oh-my-zsh ]]; then
+  error "Oh My Zsh is not installed. Please install it before running this script."
+fi
+
+# Set working dir
+cd ~/dotfiles/.files || error "No .files directory found"
+
+# Get all the necessary dotfiles
+dotfiles_dir="${HOME}/dotfiles/.files"
+dotfiles=$( find "$dotfiles_dir" -depth 1 -not -path "${dotfiles_dir}/.files/.config" && find "$dotfiles_dir" -depth 2)
+
+echo "Symlinking dotfiles..."
+for file in $dotfiles; do
+  new_file="${file#"$dotfiles_dir/"}"
+  new_file="${new_file/#./${HOME}/.}"
+  ln -sf "$file" "$new_file"
+done
