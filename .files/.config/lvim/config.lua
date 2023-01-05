1505d32146858c82c4df2c4935f7f75cb17b2207
Pawel Protas <pawel@hackerone.com>
2023-01-05 19:13:05 +0100

    feat(chafa, lvim): funny image viewer in vim

--- .files/.config/lvim/config.lua
+++ .files/.config/lvim/config.lua
@@ -386,6 +386,24 @@ lvim.plugins = {
     "felipec/vim-sanegx",
     event = "BufRead",
   },
+  {
+    "princejoogie/chafa.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "m00qek/baleia.nvim"
+    },
+    config = function()
+      require("chafa").setup({
+        render = {
+          min_padding = 5,
+          show_label = true,
+        },
+        events = {
+          update_on_nvim_resize = true,
+        },
+      })
+    end
+  },
   -- Database
   {
     "tpope/vim-dadbod"
