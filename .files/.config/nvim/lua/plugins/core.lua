9e4fbe344aedb6c05da11b1be2ba3a8ad5d893db
Pawel Protas <contact@pawelprotas.com>
2023-10-27 20:55:44 +0200

    feat(lvim): use some nice italics and bolds

--- .files/.config/nvim/lua/plugins/core.lua
+++ .files/.config/nvim/lua/plugins/core.lua
@@ -1,7 +1,26 @@
 return {
   "kvrohit/rasmus.nvim",
   "rebelot/kanagawa.nvim",
-  "olimorris/onedarkpro.nvim",
+  {
+    "olimorris/onedarkpro.nvim",
+    opts = {
+      styles = {
+        types = "NONE",
+        methods = "NONE",
+        numbers = "NONE",
+        strings = "NONE",
+        comments = "italic",
+        keywords = "bold,italic",
+        constants = "bold",
+        functions = "NONE",
+        operators = "NONE",
+        variables = "NONE",
+        parameters = "NONE",
+        conditionals = "italic",
+        virtual_text = "italic",
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
