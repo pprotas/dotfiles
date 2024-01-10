4a2157c253f479c675874c59234b962d5dd39389
Pawel Protas <contact@pawelprotas.com>
2024-01-10 15:14:55 +0100

    feat(nvim): better wrapping options

--- .files/.config/nvim/lua/config/options.lua
+++ .files/.config/nvim/lua/config/options.lua
@@ -1,9 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.breakindent = true
-vim.o.showbreak = "↪ "
 vim.o.wrap = true
+vim.o.showbreak = "↪ "
+vim.opt.cpoptions:append("n")
+vim.opt.breakindent = true
+vim.opt.breakindentopt = "shift:-2"
+vim.opt.linebreak = true
 
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
