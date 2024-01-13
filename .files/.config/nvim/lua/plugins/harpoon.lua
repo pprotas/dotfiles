c9ccc8f32d85a1be9fadb580e9dc0a0ca48dc42b
Pawel Protas <contact@pawelprotas.com>
2024-01-13 16:20:27 +0100

    feat(nvim): harpoon desc

--- .files/.config/nvim/lua/plugins/harpoon.lua
+++ .files/.config/nvim/lua/plugins/harpoon.lua
@@ -8,12 +8,14 @@ return {
         function()
           require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
         end,
+        desc = "Harpoon list",
       },
       {
         "<leader>m",
         function()
           require("harpoon"):list():append()
         end,
+        desc = "Add file to harpoon",
       },
     },
   },
