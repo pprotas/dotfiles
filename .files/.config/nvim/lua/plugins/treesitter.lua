adf62a0d40b73c24c51ac66b7a4cfb02f56bf272
Pawel Protas <contact@pawelprotas.com>
2024-04-19 14:51:38 +0200

    feat(nvim): treesitter disable in files over 100kb

--- .files/.config/nvim/lua/plugins/treesitter.lua
+++ .files/.config/nvim/lua/plugins/treesitter.lua
@@ -57,6 +57,13 @@ return {
         "graphql",
         "vimdoc",
       },
+      disable = function(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
     },
   },
 }
