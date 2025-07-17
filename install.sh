27c5f67795d7f8fc3231634b4c840b693e884f22
Pawel Protas <contact@pawelprotas.com>
2025-07-17 15:39:01 +0200

    feat(install): streamline installation script and fix command order

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 echo "[install] INFO: Installation started"
 
+sudo cp bin/gpg-sign-wrapper.sh /usr/local/bin/gpg-sign-wrapper
+sudo chmod +x /usr/local/bin/gpg-sign-wrapper
+
+git submodule update --init --recursiv
+
+~/dotfiles/symlink.sh
 if [[ "$(uname)" == "Darwin" ]]; then
 	~/dotfiles/maco.sh
 fi
-
-cp bin/gpg-sign-wrapper.sh /usr/local/bin/gpg-sign-wrapper
-sudo chmod +x /usr/local/bin/gpg-sign-wrapper
-
-~/dotfiles/symlink.sh
 
 echo "[install] INFO: Installation finished"
