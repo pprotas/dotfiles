5ec99154be89bce97a8919e498ea4e1902e778ff
Pawel Protas <contact@pawelprotas.com>
2025-07-17 15:46:02 +0200

    fix(install): correct typo in git submodule update command

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "[install] INFO: Installation started"
 sudo cp bin/gpg-sign-wrapper.sh /usr/local/bin/gpg-sign-wrapper
 sudo chmod +x /usr/local/bin/gpg-sign-wrapper
 
-git submodule update --init --recursiv
+git submodule update --init --recursive
 
 ~/dotfiles/symlink.sh
 if [[ "$(uname)" == "Darwin" ]]; then
